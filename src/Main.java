dd4e604fe96e2bf3b3a6ef87325d351449d3f1ae
yuyuzoe <zoe94040@gmail.com>
2017-09-25 14:33:51 +0800

    aaa

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("106024002游子慧");
+        System.out.println("資傳系");
+        System.out.println("hi"); }
+}
